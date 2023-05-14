5fd24a3a433166abb4586c53a6fbaa0f2142221d
brendonfeitosa <brendonfeitosa@hotmail.com>
2023-05-14 01:45:32 -0300

    Pequenos ajustes

---
+++ arquivos_site/pedidos.php
@@ -0,0 +1,41 @@
+<?php require_once("header.php"); ?>
+<div class="col-11 m-auto">
+    <h1>Histórico de Pedidos</h1>
+    <hr>
+    <br>
+    <div class="col-8 m-auto text-center"> <!-- temos que centrarlizar direito -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Número do pedido</th>
+                    <th scope="col">Data do pedido</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Total</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Jacob</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<?php require_once("footer.php") ?>

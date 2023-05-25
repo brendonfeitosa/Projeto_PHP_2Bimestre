0701567e66a24f62c7ed46f85432f7fa5528b522
andermsilva <andermarsil@gmail.com>
2023-05-25 00:19:39 -0300

    login adm

---
+++ arquivos_site/adm/login_adm.php
@@ -0,0 +1,83 @@
+<?php
+require("header.inc.php");
+require_once("../utils/connetion.php");
+
+
+$username = $password = $login_err = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (empty($_POST['email']) || empty($_POST['senha'])) {
+
+        $login_err = "Usário ou senha invalidos";
+    } else {
+        $email = $_POST['email'];
+        $password = $_POST['senha'];
+
+
+        $sql = "select adm_id, nome,senha from user where email = '{$email}'";
+
+        echo $sql;
+        $result = $conn->query($sql);
+        $data = mysqli_fetch_array($result);
+
+        if (!$data) {
+            $login_err = "ususario não encontrado";
+        } else {
+
+
+            if (password_verify($password, $data['senha'])) {
+                session_start();
+                $_SESSION['login_adm'] = true;
+                $_SESSION['id'] = $data['adm_id'];
+                $_SESSION['nome'] = $data['nome'];
+                $_SESSION['email'] = $email;
+                mysqli_close($conn);
+
+                header("Location: admin.php");
+            } else {
+
+                $login_err = "usuário não encontrado";
+            }
+        }
+    }
+}
+?>
+
+<body>
+    <main><br />
+        <span><?= $login_err ?></span>
+        <h2>Login Administrativo</h2>
+        <p>Por favor insira os dados pra realizar o Login</p>
+        <hr>
+        <section>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Usuário:</label>
+                    <input type="text" name="email" required class="form-control">
+                    <span class="invalid-feedback"></span>
+                </div>
+                <div class="form-group">
+                    <label>Senha:</label>
+                    <input type="password" name="senha" required class="form-control">
+                    <span class="invalid-feedback"></span>
+                </div><br />
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </div>
+                <p>Não possui uma conta? <a href="register.php">Registre-se agora</a>.</p>
+            </form>
+
+        </section>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+    </main>
+
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe' crossorigin='anonymous'></script>
+    <script src="../bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
+
+    <?php require("../footer.php"); ?>
+</body>
+
+</html>

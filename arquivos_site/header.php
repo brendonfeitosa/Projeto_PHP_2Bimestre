7f59dbaaed326345a847c454ab8dda3429bf4c4e
eveelinmariana <evelinmariana1@hotmail.com>
2023-05-30 11:51:56 -0300

    tes
    teste

--- arquivos_site/header.php
+++ arquivos_site/header.php
@@ -45,6 +45,17 @@
                     </li>
                 </ul>
             </div>
+        <nav class="navbar bg-body-tertiary bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand"></a>
+                <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
         </div>
+        
     </nav>
+    
     <br><br><br><br>

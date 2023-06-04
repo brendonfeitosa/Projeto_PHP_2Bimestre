66f03e13160fd21e3c866a89a112d5ae5df4523a
SuzanaSousa <suzsousa10@gmail.com>
2023-06-04 13:00:52 -0300

    fix Deuas nos ajude

--- arquivos_site/header.php
+++ arquivos_site/header.php
@@ -1,72 +1,74 @@
-<?php
-session_start();
-require_once("utils/connetion.php");
+<?php session_start();  
+    require_once("utils/connetion.php");
 ?>
 <!doctype html>
 <html lang="pt-br">
 
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fatec Food</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/styles.css">
-    <link href="bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css" rel="stylesheet">
+   
+    <link rel="stylesheet" href="./CSS/styles.css" />
 </head>
 
-<body>
+<body >
     <!-- navbar -->
-    <nav class="navbar position-fixed col-12 navbar-expand-lg navbar-dark bg-dark" style="z-index: 999;">
+    
+    <br><br><br><br>
+  
+    <nav class="navbar navbar-expand-lg fixed-top" style="background-color:#ee8f54; z-index: 1000;">
         <div class="container-fluid col-11 m-auto">
             <a class="navbar-brand" href="#"><img src="./img/logo_size_invertg40.jpg" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon">
-                </span>
+            <button class="navbar-toggler btn-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    <li class="nav-item  cor ">
+                        <a class="nav-link fw-bolder " aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="sobre_nos.php">Nossa história</a>
+                        <a class="nav-link fw-bolder fw-bolder" href="sobre_nos.php"> Nossa história</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contato.php">Contato</a>
+                        <a class="nav-link fw-bolder" href="contato.php">Contato</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="pedidos.php">Pedidos</a>
+                        <a class="nav-link  fw-bolder" href="pedidos.php">Pedidos</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="cardapio.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Cardápio
-                        </a>
+                        <a class="nav-link dropdown-toggle active" href="cardapio.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cardápio</a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="cardapio.php">Cardápio</a></li>
+                            <li><a class="dropdown-item fw-bolder" href="cardapio.php">Cardápio</a></li>
                             <hr>
-                            <li><a class="dropdown-item" href="cardapio.php#massas">Massas</a></li>
-                            <li><a class="dropdown-item" href="cardapio.php#pizzas">Pizzas</a></li>
-                            <li><a class="dropdown-item" href="cardapio.php#bebidas">Bebidas</a></li>
-                            <li><a class="dropdown-item" href="cardapio.php#sobremesas">Sobremesas</a></li>
+                            <li><a class="dropdown-item fw-bolder font" href="cardapio.php#massas">Massas</a></li>
+                            <li><a class="dropdown-item fw-bolder" href="cardapio.php#pizzas">Pizzas</a></li>
+                            <li><a class="dropdown-item fw-bolder " href="cardapio.php#sobremesas">Sobremesas</a></li>  
                         </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
 
+                        
+                    </li>
+                    
+                  
+                </ul>
+                
+            </div>
+           
+        </div>
         <?php
-        if (isset($_SESSION['login']) == true) { ?>
-            <div class="font_menor reset_decor nm_sessao">
+            if (isset($_SESSION['login']) == true) { ?> <div class="font_menor reset_decor nm_sessao">
                 <?= substr($_SESSION['nome'], 0, 9) ?> |
                 <a class="reset_decor" href="logout.php">Sair &nbsp;</a>
-            </div>
-        <?php } else { ?>
-            <div class="font_menor reset_decor nm_sessao">
+                </div>
+            <?php } else { ?>
+                <div class="font_menor reset_decor nm_sessao">
 
-                <a class="reset_decor" href="login.php ">Login</a>&nbsp;&nbsp;
-            </div>
-        <?php }
+                    <a class="reset_decor" href="login.php ">Login</a>&nbsp;&nbsp;
+                </div>
+            <?php }
         ?>
         </p>
     </nav>
-    <br><br><br><br>
--- arquivos_site/index.php
+++ arquivos_site/index.php
@@ -1,88 +1,152 @@
-<?php require_once("header.php"); ?>
+<?php require_once('./header.php'); ?>
 
-<!-- carrosel -->
-<div id="carouselExampleIndicators" class="carousel slide col-11 m-auto" data-bs-ride="true">
-    <div class="carousel-indicators" style="margin-bottom: 0;">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="./img/pizza1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./img/lasanha1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./img/massa1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./img/pudim2.jpg" class="d-block w-100" alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
-<br><br>
+
+
+   
+        
+    <!-- Carousel -->
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
+        </div>
+
+        <!-- The slideshow/carousel -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/pizza1.jpg" alt="Los Angeles" class="d-block img_banner">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/lasanha1.jpg" alt="Chicago" class="d-block img_banner">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/massa1.jpg" alt="New York" class="d-block img_banner">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/pudim2.jpg" alt="New York" class="d-block img_banner">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/pudim.png" alt="New York" class="d-block img_banner">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/manjarf1.jpg" alt="New York" class="d-block img_banner">
+            </div>
+
+            
+        </div>
+    </div> 
+
+    <br /><br /><br />
+
+    <!-- cards -->
+    <?php
+        $sql = "select * from produto ";
+
+        $result = $conn->query($sql);
+
+
+
+
+    ?>
 
 <!-- cards -->
-<h4 class="text-center">ALGUNS DE NOSSOS PRODUTOS</h4>
+    <div class="text">
+        <h4 class="text-center">DESTAQUES</h4>
+    </div>
 
-<section class="container_card" >
-    <div class="card" style="width: 18rem;">
-        <img src="https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?fit=1024%2C585&ssl=1" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div> 
-    <div class="card" style="width: 18rem;">
-        <img src="https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?fit=1024%2C585&ssl=1" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div> 
-    <div class="card" style="width: 18rem;">
-        <img src="https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?fit=1024%2C585&ssl=1" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div> 
-    <div class="card" style="width: 18rem;">
-        <img src="https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?fit=1024%2C585&ssl=1" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div> 
+<form action="" method="post">
+    <section class="container_card">
+        <div class="row text-center">
+           <?php
+              while ($data = mysqli_fetch_array($result)) { ?>
+           
+            
+                <div class="card m-2" style="width:16rem; padding:2px; ">
+                    <h6 class="card-text1"><?= $data['nome'] ?></h6>
+                  <!--   <div class="img_tama" alt="..."> -->
 
-</section >
+                        <img src="<?= $data['image_url'] ?>"
+                        class="img_card" alt="...">
+                    <!-- </div> -->
+                    <p class="card-text"><?= substr($data['descricao'],0,40) ?></p>
+                    <!-- <div class="bts">
+                        <button type="button" class="btn2" name="btn" id="subtract">
+                            <img src="./public/icons/icons8-menos-96.svg" class="tt" width="10" alt="">
+                        </button>
+                        <input class="n-pedido" readonly type="text" id="<?= $data['codigo'] ?>" name="<?= $data['codigo'] ?>" value="" size="2">
+                        <button type="button" class="btn2" id="add" value="?" name="btn" onclick="addCarrinho();">
+                            <img src="./public/icons/mais.svg" width="10" alt="">
+                        </button>
+                    </div> -->
+                </div>
 
+           
+
+            <?php } ?>
+
+
+            <script type="text/javascript">
+                function addCarrinho(id) {
+                    // alert(cp);
+
+                    alert(id);
+                    alert(document.getElementById(id).value)
+
+                    let x = document.getElementById(id).value;
+                    if (x == "") {
+
+                        document.getElementById(id).value = 1;
+                    } else {
+
+                        document.getElementById(id).value = parseInt(x) + 1;
+                    }
+
+                } 
+                var btn = document.getElementById('add');
+                btn.onclick = function() {
+
+
+                    //alert(this.name); // alerta 'seuid'
+                    let x = document.getElementById('pro').value;
+                    if (x == "") {
+
+                        document.getElementById('pro').value = 1;
+                    } else {
+
+                        document.getElementById('pro').value = parseInt(x) + 1;
+                    }
+
+                }
+
+                var btn = document.getElementById('subtract');
+                btn.onclick = function() {
+
+
+                    //alert(this.name); // alerta 'seuid'
+                    let x = document.getElementById('pro').value;
+                    if (x > 0) {
+
+                        document.getElementById('pro').value = parseInt(x) - 1;
+                    }
+
+                } 
+            </script> 
+        </div>
+
+
+    </section>
+
+</form>
 <hr>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                           
+                     
 
 <?php require_once("footer.php") ?>

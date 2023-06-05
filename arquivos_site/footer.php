bc95737498b9bdb59fee753bbf34d61e491009d1
SuzanaSousa <suzsousa10@gmail.com>
2023-06-05 19:43:37 -0300

    rodapé

--- arquivos_site/footer.php
+++ arquivos_site/footer.php
@@ -1,16 +1,9 @@
-
-</main>
-<footer>
-    <br>
-    <div class="text-center align-text-middle" style="padding: 15px 0; margin-bottom: 0; background-color: #008E9B; color: white">
+<div class="text-center align-text-middle" style="padding: 12px ; margin-bottom:0px;;
+    background-color:#ee8f54; color: white">
         &copy; <em>2023 - Curso de PHP Fatec de Presidente Prudente/SP</em>
     </div>
-    
 </footer>
 </div>
-
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe' crossorigin='anonymous'></script>
-  <script src='bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
-
 </html>
